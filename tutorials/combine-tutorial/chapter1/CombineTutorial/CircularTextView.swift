98c0745af368d3cca2818ff8ce5edc120c127188
Kevin Cheng <kevinjohnason@gmail.com>
2019-10-20 17:04:14 -0700

    .

---
+++ tutorials/combine-tutorial/chapter1/CombineTutorial/CircularTextView.swift
@@ -0,0 +1,31 @@
+//
+//  CircularTextView.swift
+//  CombineTutorial
+//
+//  Created by kevin.cheng on 9/24/19.
+//  Copyright © 2019 Kevin-Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircularTextView: View {
+    
+    @State var text: String        
+    
+    var body: some View {
+        Text(text)
+        .font(.system(size: 14))
+        .bold()
+        .foregroundColor(Color.white)
+        .padding()
+        .background(Color.green)
+        .clipShape(Circle())
+        .shadow(radius: 1)
+    }
+}
+
+struct CircularTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularTextView(text: "A")
+    }
+}
---
+++ tutorials/combine-tutorial/chapter1/CombineTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineTutorial
+//
+//  Created by kevin.cheng on 9/24/19.
+//  Copyright © 2019 Kevin-Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CombineStreamView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
